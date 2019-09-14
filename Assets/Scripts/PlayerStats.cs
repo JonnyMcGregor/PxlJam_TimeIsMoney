19761a966506bdcc000c2aee948ea72c5ce6a054
JonnyMcGregor <jonny.mcgregor1@gmail.com>
2019-09-14 14:40:27 +1200

    Updated Scripts

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -2,42 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStats : MonoBehaviour {
-	public float startingTime = 111;
-	public int startingMoney = 522;
+public class PlayerStats : MonoBehaviour
+{
+    public float startingTime = 111;
+    public int startingMoney = 522;
     public int currentMoney;
     public float currentTime;
 
     public int minimumHeight = -10;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         Initialise();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		currentTime -= Time.deltaTime;
+    }
 
-		if (Input.GetButtonDown ("UseTime")) {
-			currentTime -= 10;
-			currentMoney += 10;
-		}
-        
-		if (Input.GetButtonDown ("UseMoney")) {
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+
+        if (Input.GetButtonDown("UseTime"))
+        {
+            currentTime -= 10;
+            currentMoney += 10;
+        }
+
+        if (Input.GetButtonDown("UseMoney"))
+        {
             Debug.Log("Input pressed");
             buyNearbyItem();
-		}
-	}
+        }
+    }
 
-    void buyNearbyItem() {
+    void buyNearbyItem()
+    {
         Collider[] nearbyItems = Physics.OverlapSphere(transform.position, 10);
-        Debug.Log("Num colliders: "+nearbyItems.Length);
-        foreach (Collider c in nearbyItems) {
+        Debug.Log("Num colliders: " + nearbyItems.Length);
+        foreach (Collider c in nearbyItems)
+        {
+            Debug.Log(c.gameObject.tag);
             if (c.gameObject.tag == "Buyable")
             {
                 BuyableDoorController itemToBuy = c.gameObject.GetComponent<BuyableDoorController>();
-                if (itemToBuy.canBuy(startingMoney))
+                if (itemToBuy.canBuy(currentMoney))
                 {
                     Debug.Log("Attempting to buy");
                     currentMoney -= itemToBuy.getCost();
@@ -45,7 +53,7 @@ public class PlayerStats : MonoBehaviour {
                 }
             }
         }
-        
+
     }
 
     //Initialise the players stats
@@ -55,12 +63,14 @@ public class PlayerStats : MonoBehaviour {
         currentTime = startingTime;
     }
 
-    public bool isDead() {
-        if (transform.position.y < minimumHeight) {
+    public bool isDead()
+    {
+        if (transform.position.y < minimumHeight)
+        {
             return true;
         }
 
-        return false;
+        return currentTime <= 0;
     }
 
 }
