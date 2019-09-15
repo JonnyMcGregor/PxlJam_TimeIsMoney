4e3fb362abe870f159810092d7ae8416867c9535
JonnyMcGregor <jonny.mcgregor1@gmail.com>
2019-09-15 17:11:37 +1200

    Added Player Model to LevelController Prefab

---
+++ Assets/Scripts/CharacterModelRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterModelRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = -1 * Input.GetAxisRaw("Horizontal");
+        float moveVertical = -1 * Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.rotation = Quaternion.LookRotation(movement);
+    }
+}
